d5b77a25bd5ca1afd2780be324fe323838f614ef
Kulpot <kulpotwahu@gmail.com>
2024-01-12 03:14:35 +0800

    ...

--- C++ typedef/CppScratchPad.cpp
+++ C++ typedef/CppScratchPad.cpp
@@ -9,10 +9,18 @@ using namespace std;
 // -------------------- C++ typedef ----------------------------------
 //ref link:https://www.youtube.com/watch?v=lrDK6aZddUo&list=PLRwVmtr-pp05LyV3bYHwrFacNSNjbUqS6&index=25
 
+typedef float FLOAT32;
+typedef double FLOAT64;
+typedef unsigned int U32;
+
 void main()
 {
-	float myFloat = 6;			// float 32bits		// FLOAT32 - explicit
-	double myDouble = 6;		// double 64bits	//	FLOAT64 - explicit
+	U32 bigInt = 23456;
+	//unsigned int bigInt = 23456;
+	FLOAT32 myFloat = 6;
+	FLOAT64 myDouble = 6;
+	//float myFloat = 6;			// float 32bits		// FLOAT32 - explicit
+	//double myDouble = 6;		// double 64bits	//	FLOAT64 - explicit
 }
 
 
